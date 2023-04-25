649e2a57949f1b5745e53378f8c0c571849970be
Rohan Kumar <roku@bu.edu>
2023-04-25 00:45:04 +0000

    Upload New File

---
+++ app/src/main/java/com/example/group12project/Chessboard.java
@@ -0,0 +1,14 @@
+package com.example.group12project;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Chessboard extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chessboard);
+    }
+}
